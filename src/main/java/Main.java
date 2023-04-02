0529c1052332dbe7ea147804cb33bcdc789e1274
raimiso01 <raimison01@gmail.com>
2023-04-02 12:30:20 -0400

    final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
-import com.br.dio.desafio.dominio.Conteudo;
-import com.br.dio.desafio.dominio.Curso;
-import com.br.dio.desafio.dominio.Mentoria;
+import com.br.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -21,10 +19,39 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devRaimison = new Dev();
+        devRaimison.setNome("Raimison");
+        devRaimison.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos incristos Raimison" + devRaimison.getConteudosIncristos());
+        devRaimison.progredir();
+        devRaimison.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos incristos Raimison" + devRaimison.getConteudosIncristos());
+        System.out.println("Conteúdos concluídos Raimison" + devRaimison.getConteudosConcluidos());
+        System.out.println("XP " + devRaimison.calcularXp());
+
+        System.out.println("------------------------");
+
+        Dev devCamila = new Dev();
+        devRaimison.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos incristos Camila" + devCamila.getConteudosIncristos());
+        devRaimison.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos incristos Camila" + devCamila.getConteudosIncristos());
+        System.out.println("Conteúdos concluídos Camila" + devRaimison.getConteudosConcluidos());
+        System.out.println("XP " + devCamila.calcularXp());
 
     }
 }
